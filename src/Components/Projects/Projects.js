2454950c213dd633d2d3cfe6d200f63ed5af8e85
Jobal <jbaldwije@gmail.com>
2020-10-12 13:38:04 -0400

    updated projects:

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import Sparrows from '../SparrowsCoffee/SCApp'
-import TLife from '../1T3Life/TLApp'
+import { Link } from 'react-router-dom';
+import Sparrows from '../SparrowsCoffee/SCApp';
+import TLife from '../1T3Life/TLApp';
+import scl from '../../Img/scl.png';
+import tll from '../../Img/tll.png';
+import tpl from '../../Img/tpl.png';
 
 import './Projects.css';
 
@@ -10,38 +14,20 @@ export default class Projects extends Component {
       <div className='projects'>
         <h1 className='projh'>Projects</h1>
         <div className='projects__content'>
-          <div className='LTBG' />
-          <div className='project__one'>
-            <div className='projContent'>
-              <div className='prObg' />
-              <h2>Sparrows Coffee</h2>
-              <p>This app allows users to access information about Sparrows Coffee. Users are able to read information, get pricing, book events, and email the company for more information.</p>
-              <p>Technologies Used: React.js, Enzyme, HTML, CSS, Node, Express, JSON, PostgreSQL</p>
-              <p>Demo:</p>
-            </div>
-            <div className='project__one__example'>
-              <Sparrows className='project__sparrows'/>
-            </div>
+          <div className='projCLI alpha'>
+            <a className='projCL' href='https://sparrowscoffeega.com/' target='_blank'>
+              <img className='projCI' src={scl} alt='sparrows coffee'/>
+            </a>
           </div>
-          <div className='projectLinkContainer'>
-            <a className='projectLinks' href='https://sparrowscoffeega.com/' target='_blank' rel='noopener noreferrer'>Live Link</a>
-            <a className='projectLinks' href='https://github.com/jobal22/sparrows-coffee' target='_blank' rel='noopener noreferrer'>GitHub Link</a>
+          <div className='projCLI beta'>
+            <a className='projCL' href='https://www.1t3life.org/' target='_blank'>
+              <img className='projCI' src={tll} alt='sparrows coffee'/>
+            </a>
           </div>
-          <div className='project__two'>
-            <div className='projContent'>
-              <div className='prTbg' />
-              <h2>1T3 Life</h2>
-              <p>This app allows users to access content for theological training purpose. The user can also download selected content in pdf format. The App displays twelve squares to select. Each square directs the user to a content page, which allows the user to view the content and download the content.</p>
-              <p>Technologies Used: React.js, Enzyme, HTML, CSS</p>
-              <p>Demo:</p>
-            </div>
-            <div className='project__two__example'>
-              <TLife className='project__1T3Life' />
-            </div>
-          </div>
-          <div className='projectLinkContainer'>
-            <a className='projectLinks' href='https://www.1t3life.org/' target='_blank' rel='noopener noreferrer'>Live Link</a>
-            <a className='projectLinks' href='https://github.com/jobal22/1T3LIFE-trial-app' target='_blank' rel='noopener noreferrer'>GitHub Link</a>
+          <div className='projCLI gamma'>
+            <a className='projCL' href='https://church-site.vercel.app/' target='_blank'>
+              <img className='projCI' src={tpl} alt='sparrows coffee'/>
+            </a>
           </div>
         </div>
       </div>
